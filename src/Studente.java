19b22b9697ea6fc9e9b3f52703eb0ac60aa3bca6
AleStoCactus <alessandropala06@gmail.com>
2024-03-11 10:01:15 +0100

    2 studenti omg

--- src/Studente.java
+++ src/Studente.java
@@ -3,12 +3,28 @@ public class Studente {
     String materiaPreferita;
     String nome;
     String cognome;
+    String profPreferito;
+    float ultimoVoto;
 
-    public Studente(int age, String name, String surname, String favoriteSubject) {
+    public Studente(int age, String name, String surname, String favoriteSubject, String favoriteProf) {
         this.eta = age;
         this.materiaPreferita = favoriteSubject;
         this.nome = name;
         this.cognome = surname;
+        this.profPreferito = favoriteProf;
+    }
+
+    public float EseguiVerifica(String nomeMateria, String prof) {
+        float voto = 0.0F;
+        this.materiaPreferita = nomeMateria;
+        this.profPreferito = prof;
+        if (materiaPreferita == "Educazione Fisica") {
+            voto += 6.0F;
+        }
+        if (profPreferito == "Marino") {
+            voto += 4.0F;
+        }
+        return voto;
     }
 
     public void StampaInfo() {
@@ -16,8 +32,8 @@ public class Studente {
     }
 
     public static void main(String[] args) {
-        Studente studente1 = new Studente(17, "Mario", "Rossi", "Educazione Fisica");
-        Studente studente2 = new Studente(18, "Rylee", "Calleri", "Qt");
+        Studente studente1 = new Studente(17, "Mario", "Rossi", "Educazione Fisica", "Imbesi");
+        Studente studente2 = new Studente(18, "Rylee", "Calleri", "Qt", "Marino");
 //        studente1.eta = 17;
 //        studente2.eta = 18;
 //        studente1.nome = "Mario";
@@ -26,6 +42,9 @@ public class Studente {
 //        studente2.cognome = "Calleri";
 //        studente1.materiaPreferita = "Educazione Fisica";
 //        studente2.materiaPreferita = "Qt";
+        studente1.EseguiVerifica(studente1.materiaPreferita, studente1.profPreferito);
+        studente2.EseguiVerifica(studente2.materiaPreferita, studente2.profPreferito);
+
         System.out.println("Dati Studente1");
         studente1.StampaInfo();
         System.out.println("Dati Studente2");
