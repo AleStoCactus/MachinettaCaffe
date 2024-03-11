e58a2fa1294871fbc1acbf96e179334e5a07bc0b
AleStoCactus <alessandropala06@gmail.com>
2024-03-11 09:32:09 +0100

    2 studenti omg

--- src/Studente.java
+++ src/Studente.java
@@ -4,13 +4,31 @@ public class Studente {
     String nome;
     String cognome;
 
-    public static void main(String[] args) {
-        Studente studente1 = new Studente();
-        studente1.eta = 17;
-        studente1.nome = "Mario";
-        studente1.cognome = "Rossi";
-        studente1.materiaPreferita = "Educazione Fisica";
+    public Studente(int age, String name, String surname, String favoriteSubject) {
+        this.eta = age;
+        this.materiaPreferita = favoriteSubject;
+        this.nome = name;
+        this.cognome = surname;
+    }
 
-        System.out.printf("Lo studente ha " + studente1.eta + " anni, si chiama " + studente1.nome + ", di cognome fa " + studente1.cognome + " e la sua materia preferita è " + studente1.materiaPreferita);
+    public void StampaInfo() {
+        System.out.printf("Lo studente ha " + this.eta + " anni, si chiama " + this.nome + ", di cognome fa " + this.cognome + " e la sua materia preferita è " + this.materiaPreferita);
+    }
+
+    public static void main(String[] args) {
+        Studente studente1 = new Studente(17, "Mario", "Rossi", "Educazione Fisica");
+        Studente studente2 = new Studente(18, "Rylee", "Calleri", "Qt");
+//        studente1.eta = 17;
+//        studente2.eta = 18;
+//        studente1.nome = "Mario";
+//        studente2.nome = "Rylee";
+//        studente1.cognome = "Rossi";
+//        studente2.cognome = "Calleri";
+//        studente1.materiaPreferita = "Educazione Fisica";
+//        studente2.materiaPreferita = "Qt";
+        System.out.println("Dati Studente1");
+        studente1.StampaInfo();
+        System.out.println("Dati Studente2");
+        studente2.StampaInfo();
     }
 }
